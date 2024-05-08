933e7d0a52c5c1e2518fd99de5342215dc7cefe3
bmsjale93 <117013312+bmsjale93@users.noreply.github.com>
2024-05-08 12:12:58 +0200

    Subiendo Hello World

---
+++ projects/00-hola-mundo/src/TwitterFollowCard.jsx
@@ -0,0 +1,32 @@
+import {useState} from 'react';
+export function TwitterFollowCard({ userName, children, initialIsFollowing }) {
+  const [isFollowing, setIsFollowing] = useState(initialIsFollowing);
+
+  const imageSrc = `https://unavatar.io/${userName}`;
+  const text = isFollowing ? "Siguiendo" : "Seguir";
+  const buttonClassName = isFollowing
+    ? "tw-followCard-button is-following"
+    : "tw-followCard-button";
+
+  const handleCick = () => {
+    setIsFollowing(!isFollowing);
+  };
+
+  return (
+    <article className="tw-followCard">
+      <header className="tw-followCard-header">
+        <img className="tw-followCard-avatar" src={imageSrc} alt="Avatar" />
+        <div className="tw-followCard-info">
+          <strong>{children}</strong>
+          <span className="tw-followCard-infoUserName">@{userName}</span>
+        </div>
+      </header>
+      <aside>
+        <button className={buttonClassName} onClick={handleCick}>
+          <span className="tw-followCard-text">{text}</span>
+          <span className="tw-followCard-stopFollow">Dejar de seguir</span>
+        </button>
+      </aside>
+    </article>
+  );
+}
